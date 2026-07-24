547a3aa5939213b22503803c49825f512b1af148
sashars381-hub <sasha.rs381@gmail.com>
2026-07-24 09:59:16 +0200

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,5 +4,6 @@ import { analyticsEvent } from './analytics'
 import { theme } from './theme'
 import { review } from './review'
 import { order } from './order'
+import { gallery } from './gallery'
 
-export const schemaTypes = [product, siteSettings, analyticsEvent, theme, review, order]
+export const schemaTypes = [product, siteSettings, analyticsEvent, theme, review, order, gallery]
